cf4924260b0d628221780fc5c3ee67e53e3e0ac3
Rafael de Freitas Brandão <rafael.f.brandao2006@gmail.com>
2026-05-15 19:20:52 -0300

    guardian_user 1.6
    Início do backend

--- back-end/server/controllers/guardians_controller.js
+++ back-end/server/controllers/guardians_controller.js
@@ -1,3 +1,25 @@
 const db = require('../db/connection.js');
 
-//Função que vai pegar todos os responsáveis cadastrados e exibir na página 
+//Função que vai exibir todos os responsáveis da tabela 
+
+exports.listaResponsaveis = async (req, res) => {
+
+    try {
+        //Puxa todos os responsáveis da tabela, ordenado por ordem alfabética
+        const[rows] = await db.query(
+            `SELECT nome, CPF, email, telefone, grau FROM responsaveis ORDER BY nome ASC`
+        );
+
+        res.json(rows);
+
+
+    } catch (err) {
+
+        console.error(err);
+        return res.status(500).json({erro:'Erro interno no servidor'});
+
+    }
+
+};
+
+exports.
