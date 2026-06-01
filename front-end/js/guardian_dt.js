00963cd2d0e73ed1083a7cea4444dd616a739925
arth-doming <domingues.arthurusa@gmail.com>
2026-06-01 19:23:47 -0300

    guardian_dt.js

---
+++ front-end/js/guardian_dt.js
@@ -0,0 +1,84 @@
+import { detalharResponsavel, editarResponsavel } from "./api.js";
+
+let idResponsavel = null;
+let modoEdicao = false;
+
+window.addEventListener('load', async () => {
+    
+    const params = new URLSearchParams(window.location.search);
+    idResponsavel = params.get('id');
+
+    if (!idResponsavel) {
+        window.location.href = 'guardian_user.html'
+    }
+
+    const dados = await detalharResponsavel(idResponsavel);
+
+    if (dados.erro) {
+
+        alert(dados.erro);
+        window.location.href = 'guardian_user.html'
+    }
+
+    document.querySelector('H2').textContent = dados.nome;
+
+    document.getElementById('nome').value          = dados.nome;
+    document.getElementById('telefone').value           = dados.telefone;
+    document.getElementById('email').value            = dados.email || '';
+    document.getElementById('grau').value          = dados.grau || '';
+
+    
+    definirBloqueio(true);
+});
+
+function definirBloqueio(bloquear) {
+
+    //Dados não editáveis
+    document.getElementById('cpf').disabled = true;
+    document.getElementById('grau').disabled = true;
+    
+
+    //Dados editáveis
+    document.getElementById('nome').disabled = bloquear;
+    document.getElementById('telefone').disabled = bloquear;
+    document.getElementById('email').disabled =bloquear;
+
+}
+
+document.getElementById('edit_button').addEventListener("click", async () => {
+
+    const btn = document.getElementById('edit_button');
+    const msg = document.getElementById('msg');
+
+    if (!modoEdicao) {
+        definirBloqueio(false);
+        btn.textContent = 'Salvar';
+        modoEdicao = true;
+        return;
+    }
+
+    const dados = {
+        nome:               document.getElementById('nome').value.trim(),
+        telefone:     document.getElementById('telefone').value.trim(),
+        email:               document.getElementById('email').value.trim(),
+    };
+
+    const resultado = await editarResponsavel(idResponsavel,dados);
+
+    if (resultado.erro) {
+
+        msg.textContent = resultado.erro;
+        return;
+
+    }
+
+    document.querySelector('H2').textContent = dados.nome;
+
+    msg.textContent = 'Dados atualizados com sucesso!!';
+    btn.textContent = 'Editar';
+
+    modoEdicao = false;
+
+    definirBloqueio(true);
+
+});
